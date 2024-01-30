e2d13293910563c9e798e4bc277946bf4a7bb771
Gabor Szabo <gabor@szabgab.com>
2024-01-30 08:24:22 +0200

    some progress on the command line tool, but then I realized I can live with the admin emails being listed in the private config file for now

--- src/cli.rs
+++ src/cli.rs
@@ -1,26 +1,46 @@
+//use std::env;
 use clap::{Parser, Subcommand};
+
+use meetings::get_database;
 
 #[derive(Subcommand)]
 enum Commands {
-    Adduser { name: Option<String> },
+    Admin {
+        #[arg(long)]
+        add: String,
+    },
 }
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version)]
 struct Cli {
-    #[arg(long, default_value_t = false)]
-    adduser: bool,
+    #[arg(long)]
+    db: String,
 
-    #[arg(long, default_value = "")]
-    username: String,
+    // #[arg(long, default_value_t = false)]
+    // adduser: bool,
+
+    // #[arg(long, default_value = "")]
+    // username: String,
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
     let args = Cli::parse();
     simple_logger::init_with_env().unwrap();
     log::info!("Starting CLI");
 
-    if args.adduser {
-        log::info!("adding user");
+    let _db = get_database(&args.db).await;
+    match args.command {
+        Some(Commands::Admin { add }) => {
+            log::info!("add: {}", add);
+        }
+        None => {
+            log::info!("There was no subcommand given");
+        }
     }
+
+    Ok(())
 }
