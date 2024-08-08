f52654308db1c8c9820db4c04ec216084c7b116d
Gabor Szabo <gabor@szabgab.com>
2024-08-08 16:35:20 +0300

    only show the admin link when the user is logged in
    TODO: only show it to administrators

--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -16,6 +16,8 @@ fn compile() {
 
 
 pub fn check_guest_menu(html: &str) {
+    assert!(!html.contains(r#"<a href="/admin" class="navbar-item">Admin</a>"#));
+
     assert!(html.contains(r#"<a href="/register" class="navbar-item">Register</a>"#));
     assert!(html.contains(r#"<a href="/login" class="navbar-item">Login</a>"#));
 
