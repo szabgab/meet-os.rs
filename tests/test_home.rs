dc5668a2480a04f1df5b13546051932e0f546a5d
Gabor Szabo <gabor@szabgab.com>
2024-08-08 17:43:54 +0300

    use unwrap to simplify test code

--- tests/test_home.rs
+++ tests/test_home.rs
@@ -6,24 +6,19 @@ fn home() {
         let url = format!("http://localhost:{port}");
         let res = reqwest::blocking::get(format!("{url}/")).unwrap();
         assert_eq!(res.status(), 200);
-        match res.text() {
-            Ok(html) => {
-                check_html(&html, "title", "Meet-OS");
-                check_html(&html, "h1", "Welcome to the Meet-OS meeting server");
-                // check_html_list(
-                //     &html,
-                //     "li",
-                //     vec![
-                //         r#"<a href="/event/1">Web development with Rocket</a>"#,
-                //         r#"<a href="/group/1">Rust Maven</a>"#,
-                //     ],
-                // );
-                check_html_list(&html, "h2", vec!["Events", "Groups"]);
-                check_guest_menu(&html);
 
-                //println!("{}", html)
-            }
-            Err(err) => assert_eq!(err.to_string(), ""),
-        }
+        let html = res.text().unwrap();
+        check_html(&html, "title", "Meet-OS");
+        check_html(&html, "h1", "Welcome to the Meet-OS meeting server");
+        // check_html_list(
+        //     &html,
+        //     "li",
+        //     vec![
+        //         r#"<a href="/event/1">Web development with Rocket</a>"#,
+        //         r#"<a href="/group/1">Rust Maven</a>"#,
+        //     ],
+        // );
+        check_html_list(&html, "h2", vec!["Events", "Groups"]);
+        check_guest_menu(&html);
     });
 }
