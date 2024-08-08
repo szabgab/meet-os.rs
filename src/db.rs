df43f11511a328fc993049df5403717410f4567f
Gabor Szabo <gabor@szabgab.com>
2024-08-08 15:39:21 +0300

    use external surrealdb in github actions as well

--- src/db.rs
+++ src/db.rs
@@ -27,7 +27,11 @@ pub fn fairing() -> AdHoc {
 ///
 /// Panics when it fails to create the database folder or set up the database.
 pub async fn get_database() -> Surreal<Client> {
-    let db = Surreal::new::<Ws>("127.0.0.1:8001").await.unwrap();
+    let address = match env::var("GITHUB_ACTION") {
+        Ok(_) => "surrealdb:8000",
+        Err(_) => "127.0.0.1:8001",
+    };
+    let db = Surreal::new::<Ws>(address).await.unwrap();
     let db_namespace =
         env::var("DATABASE_NAMESPACE").unwrap_or_else(|_| String::from("meet-os-ns"));
     let db_name = env::var("DATABASE_NAME").unwrap_or_else(|_| String::from("meet-os-ns"));
