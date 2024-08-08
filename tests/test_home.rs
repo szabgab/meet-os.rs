3d214403c5009c105f58237353a2695b481eff13
Gabor Szabo <gabor@szabgab.com>
2024-08-08 17:42:50 +0300

    use unwrap to simplify test code

--- tests/test_home.rs
+++ tests/test_home.rs
@@ -4,32 +4,26 @@ use utilities::{check_guest_menu, check_html, check_html_list, run_external};
 fn home() {
     run_external(|port| {
         let url = format!("http://localhost:{port}");
-        match reqwest::blocking::get(format!("{url}/")) {
-            Ok(res) => {
-                assert_eq!(res.status(), 200);
-                match res.text() {
-                    Ok(html) => {
-                        check_html(&html, "title", "Meet-OS");
-                        check_html(&html, "h1", "Welcome to the Meet-OS meeting server");
-                        // check_html_list(
-                        //     &html,
-                        //     "li",
-                        //     vec![
-                        //         r#"<a href="/event/1">Web development with Rocket</a>"#,
-                        //         r#"<a href="/group/1">Rust Maven</a>"#,
-                        //     ],
-                        // );
-                        check_html_list(&html, "h2", vec!["Events", "Groups"]);
-                        check_guest_menu(&html);
+        let res = reqwest::blocking::get(format!("{url}/")).unwrap();
+        assert_eq!(res.status(), 200);
+        match res.text() {
+            Ok(html) => {
+                check_html(&html, "title", "Meet-OS");
+                check_html(&html, "h1", "Welcome to the Meet-OS meeting server");
+                // check_html_list(
+                //     &html,
+                //     "li",
+                //     vec![
+                //         r#"<a href="/event/1">Web development with Rocket</a>"#,
+                //         r#"<a href="/group/1">Rust Maven</a>"#,
+                //     ],
+                // );
+                check_html_list(&html, "h2", vec!["Events", "Groups"]);
+                check_guest_menu(&html);
 
-                        //println!("{}", html)
-                    }
-                    Err(err) => assert_eq!(err.to_string(), ""),
-                };
+                //println!("{}", html)
             }
-            Err(err) => {
-                assert_eq!(err.to_string(), "");
-            }
-        };
+            Err(err) => assert_eq!(err.to_string(), ""),
+        }
     });
 }
