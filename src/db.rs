556041c704b72d43a01de2d6142f1ac5cd920bfb
Gabor Szabo <gabor@szabgab.com>
2024-08-20 11:48:30 +0300

    refactor: use dbh instead of db for the database so we'll be able to use the db crate name

--- src/db.rs
+++ src/db.rs
@@ -17,8 +17,8 @@ use crate::{Counter, Event, Group, Membership, User};
 pub fn fairing() -> AdHoc {
     // TODO handle errors here properly by using AdHoc::try_on_ignite instead of AdHoc::on_ignite.
     AdHoc::on_ignite("Managed Database Connection", |rocket| async {
-        let db = get_database().await;
-        rocket.manage(db)
+        let dbh = get_database().await;
+        rocket.manage(dbh)
     })
 }
 
@@ -27,59 +27,59 @@ pub fn fairing() -> AdHoc {
 /// Panics when it fails to create the database folder or set up the database.
 pub async fn get_database() -> Surreal<Client> {
     let address = "127.0.0.1:8001";
-    let db = Surreal::new::<Ws>(address).await.unwrap();
+    let dbh = Surreal::new::<Ws>(address).await.unwrap();
     let db_namespace =
         env::var("DATABASE_NAMESPACE").unwrap_or_else(|_| String::from("meet-os-ns"));
     let db_name = env::var("DATABASE_NAME").unwrap_or_else(|_| String::from("meet-os-ns"));
-    db.use_ns(&db_namespace).use_db(&db_name).await.unwrap();
+    dbh.use_ns(&db_namespace).use_db(&db_name).await.unwrap();
     // TODO: do this only when we create the database
-    db.query("DEFINE INDEX user_email ON TABLE user COLUMNS email UNIQUE")
+    dbh.query("DEFINE INDEX user_email ON TABLE user COLUMNS email UNIQUE")
         .await
         .unwrap()
         .check()
         .unwrap();
 
-    db.query("DEFINE INDEX member_ship ON TABLE membership COLUMNS uid, gid UNIQUE")
+    dbh.query("DEFINE INDEX member_ship ON TABLE membership COLUMNS uid, gid UNIQUE")
         .await
         .unwrap()
         .check()
         .unwrap();
-    db
+    dbh
 }
 
-pub async fn add_user(db: &Surreal<Client>, user: &User) -> surrealdb::Result<()> {
+pub async fn add_user(dbh: &Surreal<Client>, user: &User) -> surrealdb::Result<()> {
     rocket::info!("add user email: '{}' code: '{}'", user.email, user.code);
 
-    db.create(Resource::from("user")).content(user).await?;
+    dbh.create(Resource::from("user")).content(user).await?;
 
     Ok(())
 }
 
-pub async fn add_event(db: &Surreal<Client>, event: &Event) -> surrealdb::Result<()> {
+pub async fn add_event(dbh: &Surreal<Client>, event: &Event) -> surrealdb::Result<()> {
     rocket::info!("add event: '{}' code: '{}'", event.eid, event.title);
 
-    db.create(Resource::from("event")).content(event).await?;
+    dbh.create(Resource::from("event")).content(event).await?;
 
     Ok(())
 }
 
-pub async fn add_group(db: &Surreal<Client>, group: &Group) -> surrealdb::Result<()> {
+pub async fn add_group(dbh: &Surreal<Client>, group: &Group) -> surrealdb::Result<()> {
     rocket::info!("add group: '{}'", group.name);
 
-    db.create(Resource::from("group")).content(group).await?;
+    dbh.create(Resource::from("group")).content(group).await?;
 
     Ok(())
 }
 
 pub async fn verify_code(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     process: &str,
     code: &str,
 ) -> surrealdb::Result<Option<User>> {
     rocket::info!("verification code: '{code}' process = '{process}'");
     let verified = true;
 
-    let mut response = db
+    let mut response = dbh
         .query("UPDATE ONLY user SET verified=$verified, code='' WHERE code=$code AND process=$process;")
         .bind(("verified", verified))
         .bind(("code", code))
@@ -100,10 +100,10 @@ pub async fn verify_code(
     Ok(entry)
 }
 
-pub async fn get_user_by_id(db: &Surreal<Client>, uid: usize) -> surrealdb::Result<Option<User>> {
+pub async fn get_user_by_id(dbh: &Surreal<Client>, uid: usize) -> surrealdb::Result<Option<User>> {
     rocket::info!("get_user_by_id: '{uid}'");
 
-    let mut response = db
+    let mut response = dbh
         .query("SELECT * FROM user WHERE uid=$uid;")
         .bind(("uid", uid))
         .await?;
@@ -118,12 +118,12 @@ pub async fn get_user_by_id(db: &Surreal<Client>, uid: usize) -> surrealdb::Resu
 }
 
 pub async fn get_user_by_email(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     email: &str,
 ) -> surrealdb::Result<Option<User>> {
     rocket::info!("get_user_by_email: '{email}'");
     rocket::info!("has db");
-    let mut response = db
+    let mut response = dbh
         .query("SELECT * FROM user WHERE email=$email;")
         .bind(("email", email))
         .await?;
@@ -138,7 +138,7 @@ pub async fn get_user_by_email(
 }
 
 pub async fn add_login_code_to_user(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     email: &str,
     process: &str,
     code: &str,
@@ -146,7 +146,7 @@ pub async fn add_login_code_to_user(
     rocket::info!("add_login_code_to_user: '{email}', '{process}', '{code}'");
 
     rocket::info!("has db");
-    let mut response = db
+    let mut response = dbh
         .query("UPDATE user SET code=$code, process=$process WHERE email=$email;")
         .bind(("email", email))
         .bind(("process", process))
@@ -163,8 +163,8 @@ pub async fn add_login_code_to_user(
 }
 
 #[must_use]
-pub async fn get_events_by_group_id(db: &Surreal<Client>, gid: usize) -> Vec<Event> {
-    match get_events(db).await {
+pub async fn get_events_by_group_id(dbh: &Surreal<Client>, gid: usize) -> Vec<Event> {
+    match get_events(dbh).await {
         Ok(events) => events
             .into_iter()
             .filter(|event| event.group_id == gid)
@@ -173,9 +173,9 @@ pub async fn get_events_by_group_id(db: &Surreal<Client>, gid: usize) -> Vec<Eve
     }
 }
 
-pub async fn get_users(db: &Surreal<Client>) -> surrealdb::Result<Vec<User>> {
+pub async fn get_users(dbh: &Surreal<Client>) -> surrealdb::Result<Vec<User>> {
     rocket::info!("get_users");
-    let mut response = db.query("SELECT * FROM user;").await?;
+    let mut response = dbh.query("SELECT * FROM user;").await?;
     let entries: Vec<User> = response.take(0)?;
     for ent in &entries {
         rocket::info!("user name {}", ent.name);
@@ -183,9 +183,9 @@ pub async fn get_users(db: &Surreal<Client>) -> surrealdb::Result<Vec<User>> {
     Ok(entries)
 }
 
-pub async fn get_groups(db: &Surreal<Client>) -> surrealdb::Result<Vec<Group>> {
+pub async fn get_groups(dbh: &Surreal<Client>) -> surrealdb::Result<Vec<Group>> {
     rocket::info!("get_groups");
-    let mut response = db.query("SELECT * FROM group;").await?;
+    let mut response = dbh.query("SELECT * FROM group;").await?;
     let entries: Vec<Group> = response.take(0)?;
     for ent in &entries {
         rocket::info!("group name {}", ent.name);
@@ -194,12 +194,12 @@ pub async fn get_groups(db: &Surreal<Client>) -> surrealdb::Result<Vec<Group>> {
 }
 
 pub async fn get_groups_by_membership_id(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     uid: usize,
 ) -> surrealdb::Result<Vec<Group>> {
     rocket::info!("get_groups_by_membership_id: '{uid}'");
 
-    // let mut response = db
+    // let mut response = dbh
     // .query("SELECT * FROM membership WHERE uid=$uid;")
     // .bind(("uid", uid))
     // .await?;
@@ -207,7 +207,7 @@ pub async fn get_groups_by_membership_id(
     // let entries: Vec<Membership> = response.take(0)?;
     // rocket::info!("gids: {entries:?}");
 
-    // let mut response = db
+    // let mut response = dbh
     // .query("SELECT gid FROM membership WHERE uid=$uid;")
     // .bind(("uid", uid))
     // .await?;
@@ -215,18 +215,18 @@ pub async fn get_groups_by_membership_id(
     // let entries: Vec<String> = response.take(0)?;
     // rocket::info!("gids: {entries:?}");
 
-    // let mut response = db
+    // let mut response = dbh
     //     .query("SELECT * FROM group WHERE gid IN (SELECT gid FROM membership WHERE uid=$uid);")
     //     .bind(("uid", uid))
     //     .await?;
 
-    // let mut response = db
+    // let mut response = dbh
     //     .query("SELECT * FROM group, membership WHERE group.gid=membership.gid AND membership.uid=$uid;")
     //     .bind(("uid", uid))
     //     .await?;
 
     // TODO: make the code above with subexpression work
-    let mut response = db
+    let mut response = dbh
         .query("SELECT * FROM membership WHERE uid=$uid;")
         .bind(("uid", uid))
         .await?;
@@ -237,7 +237,7 @@ pub async fn get_groups_by_membership_id(
     let mut groups = vec![];
     for member in memberships {
         rocket::info!("gid: {}", member.gid);
-        let mut response2 = db
+        let mut response2 = dbh
             .query("SELECT * FROM group WHERE gid=$gid;")
             .bind(("gid", member.gid))
             .await?;
@@ -251,11 +251,11 @@ pub async fn get_groups_by_membership_id(
 }
 
 pub async fn get_groups_by_owner_id(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     uid: usize,
 ) -> surrealdb::Result<Vec<Group>> {
     rocket::info!("get_groups_by_owner_id: '{uid}'");
-    let mut response = db
+    let mut response = dbh
         .query("SELECT * FROM group WHERE owner=$uid;")
         .bind(("uid", uid))
         .await?;
@@ -266,11 +266,11 @@ pub async fn get_groups_by_owner_id(
 }
 
 pub async fn get_group_by_gid(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     gid: usize,
 ) -> surrealdb::Result<Option<Group>> {
     rocket::info!("get_group_by_gid: '{gid}'");
-    let mut response = db
+    let mut response = dbh
         .query("SELECT * FROM group WHERE gid=$gid;")
         .bind(("gid", gid))
         .await?;
@@ -284,9 +284,9 @@ pub async fn get_group_by_gid(
     Ok(entry)
 }
 
-pub async fn get_events(db: &Surreal<Client>) -> surrealdb::Result<Vec<Event>> {
+pub async fn get_events(dbh: &Surreal<Client>) -> surrealdb::Result<Vec<Event>> {
     rocket::info!("get_events");
-    let mut response = db.query("SELECT * FROM event;").await?;
+    let mut response = dbh.query("SELECT * FROM event;").await?;
     let entries: Vec<Event> = response.take(0)?;
     for ent in &entries {
         rocket::info!("event name {}", ent.title);
@@ -297,14 +297,14 @@ pub async fn get_events(db: &Surreal<Client>) -> surrealdb::Result<Vec<Event>> {
 /// # Panics
 ///
 /// Panics when there is an error
-pub async fn increment(db: &Surreal<Client>, name: &str) -> surrealdb::Result<usize> {
+pub async fn increment(dbh: &Surreal<Client>, name: &str) -> surrealdb::Result<usize> {
     // TODO: do this only when creatig the database
-    let _response = db
+    let _response = dbh
         .query("DEFINE INDEX counter_name ON TABLE counter COLUMNS name UNIQUE")
         .await?;
 
     #[allow(clippy::separated_literal_suffix)]
-    let response = db
+    let response = dbh
         .query(
             "
             INSERT INTO counter (name, count)
@@ -325,11 +325,11 @@ pub async fn increment(db: &Surreal<Client>, name: &str) -> surrealdb::Result<us
 }
 
 pub async fn get_event_by_eid(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     eid: usize,
 ) -> surrealdb::Result<Option<Event>> {
     rocket::info!("get_event_by_eid: '{eid}'");
-    let mut response = db
+    let mut response = dbh
         .query("SELECT * FROM event WHERE eid=$eid;")
         .bind(("eid", eid))
         .await?;
@@ -343,7 +343,7 @@ pub async fn get_event_by_eid(
     Ok(entry)
 }
 
-pub async fn join_group(db: &Surreal<Client>, gid: usize, uid: usize) -> surrealdb::Result<()> {
+pub async fn join_group(dbh: &Surreal<Client>, gid: usize, uid: usize) -> surrealdb::Result<()> {
     rocket::info!("user {} joins group: {}", uid, gid);
 
     let membership = Membership {
@@ -352,7 +352,7 @@ pub async fn join_group(db: &Surreal<Client>, gid: usize, uid: usize) -> surreal
         admin: false,
     };
 
-    db.create(Resource::from("membership"))
+    dbh.create(Resource::from("membership"))
         .content(membership)
         .await?;
 
@@ -362,10 +362,10 @@ pub async fn join_group(db: &Surreal<Client>, gid: usize, uid: usize) -> surreal
 /// # Panics
 ///
 /// Panics when it fails
-pub async fn leave_group(db: &Surreal<Client>, gid: usize, uid: usize) -> surrealdb::Result<()> {
+pub async fn leave_group(dbh: &Surreal<Client>, gid: usize, uid: usize) -> surrealdb::Result<()> {
     rocket::info!("user {} leaves group: {}", uid, gid);
 
-    db.query("DELETE membership WHERE uid=$uid AND gid=$gid")
+    dbh.query("DELETE membership WHERE uid=$uid AND gid=$gid")
         .bind(("uid", uid))
         .bind(("gid", gid))
         .await?
@@ -376,11 +376,11 @@ pub async fn leave_group(db: &Surreal<Client>, gid: usize, uid: usize) -> surrea
 }
 
 pub async fn get_membership(
-    db: &Surreal<Client>,
+    dbh: &Surreal<Client>,
     gid: usize,
     uid: usize,
 ) -> surrealdb::Result<Option<Membership>> {
-    let mut response = db
+    let mut response = dbh
         .query("SELECT * FROM membership WHERE gid=$gid AND uid=$uid;")
         .bind(("gid", gid))
         .bind(("uid", uid))
