d20309ac56172fd3e68b20717b2669ac4aad4d04
Gabor Szabo <gabor@szabgab.com>
2024-08-27 08:51:52 +0300

    switch more routes to the new VisitorGuard

--- src/public.rs
+++ src/public.rs
@@ -1,62 +1,45 @@
-use rocket::http::CookieJar;
 use rocket::Route;
-use rocket::State;
 use rocket_dyn_templates::{context, Template};
 
-use surrealdb::engine::remote::ws::Client;
-use surrealdb::Surreal;
-
-use crate::web::Visitor;
-use crate::{get_public_config, MyConfig};
+use crate::get_public_config;
+use crate::web::VisitorGuard;
 
 pub fn routes() -> Vec<Route> {
     routes![about, privacy, soc,]
 }
 
 #[get("/about")]
-async fn about(
-    cookies: &CookieJar<'_>,
-    dbh: &State<Surreal<Client>>,
-    myconfig: &State<MyConfig>,
-) -> Template {
+fn about(visitor: VisitorGuard) -> Template {
     Template::render(
         "about",
         context! {
             title: "About Meet-OS",
             config: get_public_config(),
-            visitor: Visitor::new(cookies, dbh, myconfig).await,
+            visitor,
         },
     )
 }
 
 #[get("/privacy")]
-async fn privacy(
-    cookies: &CookieJar<'_>,
-    dbh: &State<Surreal<Client>>,
-    myconfig: &State<MyConfig>,
-) -> Template {
+fn privacy(visitor: VisitorGuard) -> Template {
     Template::render(
         "privacy",
         context! {
             title: "Privacy Policy",
             config: get_public_config(),
-            visitor: Visitor::new(cookies, dbh, myconfig).await,
+            visitor,
         },
     )
 }
 
 #[get("/soc")]
-async fn soc(
-    cookies: &CookieJar<'_>,
-    dbh: &State<Surreal<Client>>,
-    myconfig: &State<MyConfig>,
-) -> Template {
+fn soc(visitor: VisitorGuard) -> Template {
     Template::render(
         "soc",
         context! {
             title: "Standard of Conduct",
             config: get_public_config(),
-            visitor: Visitor::new(cookies, dbh, myconfig).await,
+            visitor,
         },
     )
 }
