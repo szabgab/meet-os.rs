a40cfd1338f23f1642fd2a6dd541decf790f6244
Gabor Szabo <gabor@szabgab.com>
2024-09-01 06:34:34 +0300

    add test of /admin/users

--- src/test_admin.rs
+++ src/test_admin.rs
@@ -104,8 +104,36 @@ fn admin_users_page_as_guest() {
     })
 }
 
-// /admin_users_page_as_user
-// /admin_users_page_as_admin
+#[test]
+fn admin_users_page_as_user() {
+    run_inprocess(|email_folder, client| {
+        setup_many(&client, &email_folder);
+        login_helper(&client, "foo@meet-os.com", "123foo");
+
+        let res = client.get("/admin/users").dispatch();
+        assert_eq!(res.status(), Status::Forbidden);
+        let html = res.into_string().unwrap();
+        //assert_eq!(html, "");
+        check_html(&html, "title", "Unauthorized");
+    })
+}
+
+#[test]
+fn admin_users_page_as_admin() {
+    run_inprocess(|email_folder, client| {
+        setup_many(&client, &email_folder);
+        login_helper(&client, "admin@meet-os.com", "123456");
+
+        let res = client.get("/admin/users").dispatch();
+        assert_eq!(res.status(), Status::Ok);
+        let html = res.into_string().unwrap();
+        //assert_eq!(html, "");
+        check_html(&html, "title", "List Users by Admin");
+        check_html(&html, "h1", "List Users by Admin");
+        assert!(html.contains(r#"<a href="/user/3">Foo 2</a>"#));
+        assert!(html.contains(r#"<td><a href="/user/4">Site Manager</a></td>"#));
+    })
+}
 
 #[test]
 fn admin_search_get_as_guest() {
