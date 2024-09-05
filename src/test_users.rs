d709e0df28a3c2f0fb2f54a075c4d9ada88b9b5d
Gabor Szabo <gabor@szabgab.com>
2024-09-05 04:29:18 +0300

    remove commented out and unused tests

--- src/test_users.rs
+++ src/test_users.rs
@@ -230,69 +230,6 @@ fn login_admin_user() {
     });
 }
 
-// #[test]
-// fn login() {
-//    run_inprocess(|email_folder, client| {
-
-//         register_user_helper(&client, "Foo Bar", "foo@meet-os.com", "123456");
-//         //check_profile_page_in_process(&client, &url, &cookie_str, "Peti Bar");
-
-//         let res = client
-//             .post("/login")
-// .header(ContentType::Form)
-
-//             .form(&[("email", "foo@meet-os.com")])
-//             .dispatch();
-// assert_eq!(res.status(), Status::Ok);
-// let html = res.into_string().unwrap();
-//         assert!(res.headers().get("set-cookie").is_none());
-//         check_html(&html, "title", "We sent you an email");
-//         assert!(html.contains("We sent you an email to <b>foo@meet-os.com</b>"));
-
-//         // TODO: get the user from the database and check if there is a code and if the process is "login"
-
-//         // get the email and extract the code from the link
-//         let email_file = format!("{email_folder}/0.txt");
-//         let email = std::fs::read_to_string(email_file).unwrap();
-//         // https://meet-os.com/verify/login/c0514ec6-c51e-4376-ae8e-df82ef79bcef
-//         let re = Regex::new("http://localhost:[0-9]+/verify/login/([a-z0-9-]+)").unwrap();
-
-//         log::info!("email: {email}");
-//         let code = match re.captures(&email) {
-//             Some(value) => value[1].to_owned(),
-//             None => panic!("Code not found in email"),
-//         };
-//         println!("code: {code}");
-//         //assert_eq!(code, res.code);
-
-//         // "Click" on the link an verify the email
-//         let res = client
-//             .get("/verify/login/{code}")
-//             .dispach();
-// assert_eq!(res.status(), Status::Ok);
-// let html = res.into_string().unwrap();
-//         let cookie_str = extract_cookie(&res);
-//         //assert_eq!(html, "x");
-//         check_html(&html, "title", "Welcome back");
-//         check_user_menu(&html);
-
-//         // Access the profile with the cookie
-//         check_profile_page_in_process(&client, &url, &cookie_str, "Foo Bar");
-
-//         let res = client.get("/logout").dispatch();
-// assert_eq!(res.status(), Status::Ok);
-// let html = res.into_string().unwrap();
-//         check_html(&html, "title", "Logged out");
-//         check_html(&html, "h1", "Logged out");
-
-//         // TODO as the login information is only saved in the client-side cookie, if someone has the cookie they can
-//         // use it even the user has clicked on /logout and we have asked the browser to remove the cookie.
-//         // If we want to make sure that the user cannot access the system any more we'll have to manage the login information
-//         // on the server side.
-//         //check_profile_page_in_process(&client, &url, &cookie_str, "");
-//     });
-// }
-
 #[test]
 fn register_with_bad_email_address() {
     run_inprocess(|email_folder, client| {
@@ -317,36 +254,6 @@ fn register_with_bad_email_address() {
         check_guest_menu(&html);
     });
 }
-
-// #[test]
-// fn event_page() {
-//    run_inprocess(|email_folder, client| {
-//         let res = client.get("/event/1").dispatch();
-// assert_eq!(res.status(), Status::Ok);
-// let html = res.into_string().unwrap();
-//         check_html(&html, "title", "Web development with Rocket");
-//         check_html(&html, "h1", "Web development with Rocket");
-//         assert!(html.contains(r#"Organized by <a href="/group/1">Rust Maven</a>."#));
-//         assert!(html.contains("<div><b>Date</b>: 2024-02-04T17:00:00 UTC</div>"));
-//         assert!(html.contains("<div><b>Location</b>: Virtual</div>"));
-//     });
-// }
-
-// #[test]
-// fn group_page() {
-//    run_inprocess(|email_folder, client| {
-//         let res = client.get("/group/1").dispatch();
-// assert_eq!(res.status(), Status::Ok);
-// let html = res.into_string().unwrap();
-
-//         check_html(&html, "title", "Rust Maven");
-//         check_html(&html, "h1", "Rust Maven");
-//         assert!(html.contains(
-//             r#"<li><a href="/event/1">2024-02-04T17:00:00 - Web development with Rocket</a></li>"#
-//         ));
-//         assert!(html.contains("<div><b>Location</b>: Virtual</div>"));
-//     });
-// }
 
 // TODO try to login with an email address that was not registered
 
